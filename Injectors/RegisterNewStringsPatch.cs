9480350946aa64aeb6fe1afe21039abfb476f441
Vegard Dæmring <44493351+Veggissss@users.noreply.github.com>
2025-07-16 19:45:50 +0200

    feat: added title for challenge screen.

---
+++ Injectors/RegisterNewStringsPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace BadAppleTrickyTowersMod.Injectors
+{
+    [HarmonyPatch(typeof(LanguageManager))]
+	[HarmonyPatch("LoadLanguage")]
+	class RegisterNewStringsPatch
+	{
+		static void Postfix(LanguageManager __instance)
+		{
+            __instance.RegisterLanguage("STATE_TITLE_WORLD_NAME_BAD_APPLE", "Bad Apple", false, true);
+            return;
+		}
+	}
+}
