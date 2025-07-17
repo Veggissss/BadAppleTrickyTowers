2d9016490b74516996daf373c1d2452c6e9c3d36
Vegard Dæmring <44493351+Veggissss@users.noreply.github.com>
2025-07-17 23:44:06 +0200

    feat: added config for spawnable bricks

--- Injectors/HarmonySetup.cs
+++ Injectors/HarmonySetup.cs
@@ -1,5 +1,8 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
+using System.Linq;
+using UnityEngine;
 
 namespace TrickyMultiplayerPlus
 {
@@ -7,10 +10,21 @@ namespace TrickyMultiplayerPlus
     [BepInPlugin("badapple", "BadApple", "1.0")]
 	public class TrickyTowersModPlugin : BaseUnityPlugin
 	{
-		void Awake()
+        public static ConfigEntry<string> BricksetConfig;
+        public static string[] Brickset => BricksetConfig.Value.Split(',').Select(s => s.Trim()).ToArray();
+
+        void Awake()
 		{
 			var harmony = new Harmony("badapple");
 			harmony.PatchAll();
-		}
+
+            BricksetConfig = Config.Bind(
+                "General",
+                "Brickset",
+                "BRICK_O",
+                "Comma-separated list of brick IDs to use (e.g., BRICK_T,BRICK_L,BRICK_J,BRICK_O,BRICK_I,BRICK_S,BRICK_Z)"
+            );
+            Debug.Log($"Loaded brickset: {string.Join(", ", Brickset)}");
+        }
 	}
 }
